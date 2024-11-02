e019b31bb061b9a353bb9f70d16dec822b625e0e
admin <admin@DESKTOP-8VQH0LB>
2024-11-02 09:20:50 -0700

    Add project files.

---
+++ TASK4LAB06/TASK4LAB06.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	for (int i = 1; i <= 3; i++) {
+		cout << "week :" << i<< endl;
+		for (int j = 1; j <= 7; j++) {
+			cout << "DAYS :" << j << endl;
+		}
+}
+	return 0;
+}
